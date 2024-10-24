9ac4cbdd6b0d5288d1f5faf0f64f6bdcb30233a4
Men6d656e <men6d656e@gmail.com>
2024-10-24 10:20:45 +0500

    second commit

---
+++ fileOne.py
@@ -0,0 +1,61 @@
+
+# # name = "akash";
+# # print(name);
+
+
+# my_string = 'my naMe is akash'
+# print(my_string);
+# type(my_string)
+# print(my_string[0])
+# print(my_string[1])
+# print(my_string[4])
+# print(my_string[4:7])
+# len(my_string)
+# a = my_string.lower()
+# b = my_string.upper()
+# print(a)
+# print(b)
+
+
+# # replace method not working
+# # z = my_string,repr('my' , 'im')
+
+# # print(z)
+
+# thisString = ' hello hello hello hello world'
+
+# print(thisString.count('hello'))
+
+
+# # find index
+
+# s1 = 'this is akash!!!'
+# print(s1.find('akash'))
+
+# # spliting string
+# s2 = 'this! is akash! akash! akash!'
+# print(s2.split('!'))
+
+
+
+
+# tupple
+
+# tup = (1,2,3,4,5,"a","s")
+# print(tup)
+# print(type(tup))
+
+# print(tup[0])
+# print(tup[2])
+# print(tup[-1])
+# print(tup[1:4])
+
+
+tup1 = (1,2,3)
+tup2 = (4,5,6)
+x = tup1 + tup2
+print(x)
+print(min(x))
+print(max(x))
+print(x*4)
+print(len(x))
