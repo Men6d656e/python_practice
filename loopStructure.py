b28746352ac4e558ea5274f643b68924d94253ae
Men6d656e <men6d656e@gmail.com>
2024-10-26 22:36:42 +0500

    cover loop

---
+++ loopStructure.py
@@ -0,0 +1,22 @@
+
+
+i = 0;
+
+while i<=10:
+    print(i)
+    i = i+1
+
+list = [1,2,3,4,5,6]
+k = 0
+# while loop
+print("while loop")
+while k < len(list):
+    print(list[k])
+    k = k+1
+# for loop
+
+print("for loop")
+for i in list:
+    print(i)
+
+
